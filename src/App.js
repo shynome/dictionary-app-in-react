e39d0ebea15055da3268a745cb3b3fd0fdfac111
libregd <libregd@outlook.com>
2022-04-30 17:09:54 +0800

    basic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import Footer from "./component/Footer"
+import Defination from "./component/Defination";
 import './App.css';
+import React, { useState, useEffect } from "react";
+const App =()=> {
+  const [word, setWord] = useState();
+  const [mean, setMean] = useState([]);
+  const [main, setMain] = useState([]);
+  const [audio, setAudio] = useState();
 
-function App() {
+  const dataApi = async () => {
+    const data = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+    const dataJ = await data.json();
+    setMean(dataJ);
+    // console.log(dataJ);
+    setMain(dataJ[0]);
+    // console.log(dataJ[0]);
+    const url = dataJ[0].phonetics[0].audio;
+    setAudio(url);
+  };
+  useEffect(() => {
+    dataApi();
+  }, []);
+
+
+  const Search = () => {
+    dataApi();
+    setWord("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container-fluid "> 
+        <div className="row">
+          <div className="col-12 text-center fw-bold fs-1 p-3 bg-primary  bg-opacity-10 text-primary" >
+              English Dictionary
+            </div>
+            <div className="form-floating bg-primary  bg-opacity-10 py-3 pb-5 d-flex justify-content-center">
+              <input
+                type="text"
+                className="form-control-sm border-1 border-primary px-2 col-md-3 col-sm-4"
+                placeholder="Type your word"
+                id="floatingInput"
+                input={word}
+                onChange={(e)=>setWord(e.target.value)}
+               
+              />
+              <button
+                className="btn btn-primary text-light col-md-1 col-sm-2 mx-2"
+                onClick={Search}
+              >
+               Search
+              </button>
+          </div>
+      </div>       
+      </div>
+      {word === "" ? (
+        <Defination mean={mean} main={main} audio={audio} />
+      ) : (
+        <div className="fs-4 text-capitalize text-center fw-light text-secondary extra mt-5" >
+          type a word in the box
+        </div>
+      )}
+      <Footer />
+    </>
   );
 }
 
---
+++ src/component/Defination.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Select from "./Select.js";
+
+// import Synonyms from "./Synonym";
+// import Antonyms from "./Antonyms";
+
+const Defination = ({ mean, main, audio }) => {
+  return (
+    <>
+    
+      <div className="container-fluid my-3">
+        <div className="row ">
+          <div className="col-12 text-center text-capitalize fs-1 fw-bold  mb-4 ">
+            {main.word}
+          </div>
+          {audio ? (
+            <audio
+              controls
+              className="color mx-auto text-center col-10 mb-3"
+              src={audio}
+            ></audio>
+          ) : (
+            <div className="color fs-3 text-center">Audio not found</div>
+          )}
+          {/* <div className="col-12 text-start my-3 text-capitalize fs-4 fw-bold">
+            meaning & definitions :
+          </div> */}
+          <div className="col-12 mx-3">
+              <Select mean={mean} />
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Defination;
